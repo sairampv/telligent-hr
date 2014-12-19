2693bb1f4fac9c1d28067f25db38a58efb601250
reddy.telligent@gmail.com <reddy.telligent@gmail.com@2fc33e4f-b29a-dd76-131f-9e2053e58947>
2014-12-19 08:46:14 +0000

    Bug Fixing

---
+++ trunk/Telligent/WebContent/view/js/popup.js
@@ -0,0 +1,54 @@
+	/* event for close the popup */
+	$("div.close").hover(
+					function() {
+						$('span.ecs_tooltip').show();
+					},
+					function () {
+    					$('span.ecs_tooltip').hide();
+  					}
+				);
+	
+	$("div.close").click(function() {
+		disablePopup();  // function close pop up
+	});
+	
+	$(this).keyup(function(event) {
+		if (event.which == 27) { // 27 is 'Ecs' in the keyboard
+			disablePopup();  // function close pop up
+		}  	
+	});
+	
+	
+	
+	
+	
+
+	 /************** start: functions. **************/
+	function loading() {
+		$("div.loader").show();  
+	}
+	function closeloading() {
+		$("div.loader").fadeOut('normal');  
+	}
+	
+	var popupStatus = 0; // set value
+	
+	function loadPopup() { 
+		if(popupStatus == 0) { // if value is 0, show popup
+			closeloading(); // fadeout loading
+			$("#dialog").fadeIn(0500); // fadein popup div
+			$("#backgroundPopup").css("opacity", "0.7"); // css opacity, supports IE7, IE8
+			$("#backgroundPopup").fadeIn(0001); 
+			popupStatus = 1; // and set value to 1
+		}	
+	}
+		
+	function disablePopup() {
+		if(popupStatus == 1) { // if value is 1, close popup
+			$("#dialog").fadeOut("normal");  
+			$("#backgroundPopup").fadeOut("normal");  
+			popupStatus = 0;  // and set value to 0
+		}
+	}
+	
+	
