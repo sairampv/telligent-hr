2b7757865aec6f87f8e3e815dc0c0d354b543c86
reddy.telligent@gmail.com <reddy.telligent@gmail.com@2fc33e4f-b29a-dd76-131f-9e2053e58947>
2014-12-26 07:35:58 +0000

    Employee Screen Changes

--- trunk/Telligent/src/com/telligent/model/daos/impl/EmployeeDAO.java
+++ trunk/Telligent/src/com/telligent/model/daos/impl/EmployeeDAO.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import org.apache.log4j.Logger;
 
 import com.telligent.core.system.annotation.SpringBean;
-import com.telligent.model.daos.IEmployeeDAO;
 import com.telligent.model.db.AbstractDBManager;
 import com.telligent.model.dtos.EmployeeDTO;
 import com.telligent.model.dtos.MapDTO;
@@ -19,7 +18,7 @@ import com.telligent.model.dtos.TeamDTO;
  *
  */
 @SpringBean
-public class EmployeeDAO extends AbstractDBManager implements IEmployeeDAO{
+public class EmployeeDAO extends AbstractDBManager{
 
 	public final Logger logger = Logger.getLogger(EmployeeDAO.class);
 
@@ -60,7 +59,6 @@ public class EmployeeDAO extends AbstractDBManager implements IEmployeeDAO{
 	 *  @param teamId
 	 * 
 	 */
-	@Override
 	public ArrayList<EmployeeDTO> getTeamEmployees(String teamId) {
 		logger.info("in getTeamEmployees DAO");
 		ArrayList<EmployeeDTO> list = new ArrayList<EmployeeDTO>();
@@ -91,7 +89,6 @@ public class EmployeeDAO extends AbstractDBManager implements IEmployeeDAO{
 		return list;
 	}
 
-	@Override
 	public boolean saveEmployeeDetails(EmployeeDTO employeeDTO) {
 		logger.info("in saveEmployeeDetails DAO");
 		Connection conn = null;
@@ -120,7 +117,6 @@ public class EmployeeDAO extends AbstractDBManager implements IEmployeeDAO{
 		return false;
 	}
 
-	@Override
 	public ArrayList<MapDTO> searchList(String firstName, String lastName,String empId) {
 		logger.info("in searchList DAO");
 		Connection conn = null;
@@ -155,7 +151,6 @@ public class EmployeeDAO extends AbstractDBManager implements IEmployeeDAO{
 		return list;
 	}
 
-	@Override
 	public ArrayList<MapDTO> searchTeamEmployees(String teamName) {
 		logger.info("in searchList DAO");
 		Connection conn = null;
@@ -182,6 +177,69 @@ public class EmployeeDAO extends AbstractDBManager implements IEmployeeDAO{
 			this.closeAll(conn, ps, rs);
 		}
 		return list;
+	}
+	public EmployeeDTO getEmployeeDetails(String empId){
+		logger.info("in getEmployeeDetails");
+		EmployeeDTO dto = new EmployeeDTO();
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		StringBuffer query = new StringBuffer();
+		try{
+			conn = this.getConnection();
+			query.append("select employee_name,last_name,email_id,employee_no,socialSecNo,badgeNo,DATE_FORMAT(dateOfBirth,'%y/%m/%d') dateOfBirth,DATE_FORMAT(effectiveDate,'%y/%m/%d') effectiveDate,citizenship, ");
+			query.append("maritalStatus,veteranStatus,visaType,DATE_FORMAT(visaExpDate,'%y/%m/%d') visaExpDate,isMinor,gender, ");
+			query.append("ethinicity,disabled,disabledDesc,homePhone,cellPhone,personalemail,addressLine1,addressLine2,city,state,zipcode, ");
+			query.append("workPhone,workExt,workEmail,workCellPhone,emergencyLastName,emergencyFirstName,emergencyRelationShip,emergencyHomePhone,emergencyCellPhone,emergencyEmail  ");
+			query.append("from employee e where employee_id=? ");
+			ps = conn.prepareStatement(query.toString());
+			ps.setString(1, empId);
+			rs = ps.executeQuery();
+			if(rs.next()){
+				dto.setEmployeeId(empId);
+				dto.setEmployeeName(rs.getString("employee_name"));
+				dto.setEmployeeNo(rs.getString("employee_no"));
+				dto.setLastName(rs.getString("last_name"));
+				dto.setEmail(rs.getString("email_id"));
+				dto.setSocialSecNo(rs.getString("socialSecNo"));
+				dto.setBadgeNo(rs.getString("badgeNo"));
+				dto.setDateOfBirth(rs.getString("dateOfBirth"));
+				dto.setEffectiveDate(rs.getString("effectiveDate"));
+				dto.setCitizenship(rs.getString("citizenship"));
+				dto.setMaritalStatus(rs.getString("maritalStatus"));
+				dto.setVeteranStatus(rs.getString("veteranStatus"));
+				dto.setVisaType(rs.getString("visaType"));
+				dto.setVisaExpDate(rs.getString("visaExpDate"));
+				dto.setMinor(rs.getString("isMinor").equalsIgnoreCase("0")?true:false);
+				dto.setGender(rs.getString("gender"));
+				dto.setEthinicity(rs.getString("ethinicity"));
+				dto.setDisabled(rs.getString("disabled"));
+				dto.setDisablityDesc(rs.getString("disabledDesc"));
+				dto.setHomePhone(rs.getString("homePhone"));
+				dto.setCellPhone(rs.getString("cellPhone"));
+				dto.setPersonalEmail(rs.getString("personalemail"));
+				dto.setAddressLine1(rs.getString("addressLine1"));
+				dto.setAddressLine2(rs.getString("addressLine2"));
+				dto.setCity(rs.getString("city"));
+				dto.setState(rs.getString("state"));
+				dto.setZipcode(rs.getString("zipcode"));
+				dto.setWorkPhone(rs.getString("workPhone"));
+				dto.setWorkExt(rs.getString("workExt"));
+				dto.setWorkEmail(rs.getString("workEmail"));
+				dto.setWorkCellPhone(rs.getString("workCellPhone"));
+				dto.setEmergencyCellPhone(rs.getString("emergencyCellPhone"));
+				dto.setEmergencyEmail(rs.getString("emergencyEmail"));
+				dto.setEmergencyFirstName(rs.getString("emergencyFirstName"));
+				dto.setEmergencyLastName(rs.getString("emergencyLastName"));
+				dto.setEmergencyHomePhone(rs.getString("emergencyHomePhone"));
+				dto.setEmergencyRelationShip(rs.getString("emergencyRelationShip"));
+			}
+		}catch (Exception ex) {
+			logger.info("Excpetion in getEmployeeDetails "+ex.getMessage());
+		} finally {
+			this.closeAll(conn, ps, rs);
+		}
+		return dto;
 	}
 
 }
--- trunk/Telligent/src/com/telligent/model/dtos/EmployeeDTO.java
+++ trunk/Telligent/src/com/telligent/model/dtos/EmployeeDTO.java
@@ -5,14 +5,49 @@ import java.util.ArrayList;
 public class EmployeeDTO {
 	
 	private String employeeId;
-	private String employeeName;
+	private String employeeNo;
+	private String employeeName; // First Name
 	private String teamId;
 	private String teamName;
 	private String lastName;
-	private String email;
 	private String salary;
 	private ArrayList<TeamDTO> teamsList = new ArrayList<TeamDTO>();
+	private String socialSecNo;
+	private String badgeNo;
+	private String dateOfBirth;
+	private String effectiveDate;
+	private String citizenship;
+	private String maritalStatus;
+	private String veteranStatus;
+	private String visaType;
+	private String visaExpDate;
+	private boolean minor;
+	private String gender;
+	private String ethinicity;
+	private String disabled;
+	private String disablityDesc;
 	
+	private String homePhone;
+	private String cellPhone;
+	private String email;
+	private String personalEmail;
+	private String addressLine1;
+	private String addressLine2;
+	private String city;
+	private String state;
+	private String zipcode;
+	
+	private String workPhone;
+	private String workExt;
+	private String workEmail;
+	private String workCellPhone;
+	
+	private String emergencyLastName;
+	private String emergencyFirstName;
+	private String emergencyRelationShip;
+	private String emergencyHomePhone;
+	private String emergencyCellPhone;
+	private String emergencyEmail;
 	/**
 	 * @return the employeeId
 	 */
@@ -24,6 +59,18 @@ public class EmployeeDTO {
 	 */
 	public void setEmployeeId(String employeeId) {
 		this.employeeId = employeeId;
+	}
+	/**
+	 * @return the employeeNo
+	 */
+	public String getEmployeeNo() {
+		return employeeNo;
+	}
+	/**
+	 * @param employeeNo the employeeNo to set
+	 */
+	public void setEmployeeNo(String employeeNo) {
+		this.employeeNo = employeeNo;
 	}
 	/**
 	 * @return the employeeName
@@ -74,18 +121,6 @@ public class EmployeeDTO {
 		this.lastName = lastName;
 	}
 	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-	/**
-	 * @param email the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	/**
 	 * @return the salary
 	 */
 	public String getSalary() {
@@ -109,5 +144,402 @@ public class EmployeeDTO {
 	public void setTeamsList(ArrayList<TeamDTO> teamsList) {
 		this.teamsList = teamsList;
 	}
+	/**
+	 * @return the socialSecNo
+	 */
+	public String getSocialSecNo() {
+		return socialSecNo;
+	}
+	/**
+	 * @param socialSecNo the socialSecNo to set
+	 */
+	public void setSocialSecNo(String socialSecNo) {
+		this.socialSecNo = socialSecNo;
+	}
+	/**
+	 * @return the badgeNo
+	 */
+	public String getBadgeNo() {
+		return badgeNo;
+	}
+	/**
+	 * @param badgeNo the badgeNo to set
+	 */
+	public void setBadgeNo(String badgeNo) {
+		this.badgeNo = badgeNo;
+	}
+	/**
+	 * @return the effectiveDate
+	 */
+	public String getEffectiveDate() {
+		return effectiveDate;
+	}
+	/**
+	 * @param effectiveDate the effectiveDate to set
+	 */
+	public void setEffectiveDate(String effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+	/**
+	 * @return the dateOfBirth
+	 */
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 */
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	/**
+	 * @return the citizenship
+	 */
+	public String getCitizenship() {
+		return citizenship;
+	}
+	/**
+	 * @param citizenship the citizenship to set
+	 */
+	public void setCitizenship(String citizenship) {
+		this.citizenship = citizenship;
+	}
+	/**
+	 * @return the maritalStatus
+	 */
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
+	/**
+	 * @param maritalStatus the maritalStatus to set
+	 */
+	public void setMaritalStatus(String maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+	/**
+	 * @return the veteranStatus
+	 */
+	public String getVeteranStatus() {
+		return veteranStatus;
+	}
+	/**
+	 * @param veteranStatus the veteranStatus to set
+	 */
+	public void setVeteranStatus(String veteranStatus) {
+		this.veteranStatus = veteranStatus;
+	}
+	/**
+	 * @return the visaType
+	 */
+	public String getVisaType() {
+		return visaType;
+	}
+	/**
+	 * @param visaType the visaType to set
+	 */
+	public void setVisaType(String visaType) {
+		this.visaType = visaType;
+	}
+	/**
+	 * @return the visaExpDate
+	 */
+	public String getVisaExpDate() {
+		return visaExpDate;
+	}
+	/**
+	 * @param visaExpDate the visaExpDate to set
+	 */
+	public void setVisaExpDate(String visaExpDate) {
+		this.visaExpDate = visaExpDate;
+	}
+	/**
+	 * @return the isMinor
+	 */
+	public boolean isMinor() {
+		return minor;
+	}
+	/**
+	 * @param isMinor the isMinor to set
+	 */
+	public void setMinor(boolean minor) {
+		this.minor = minor;
+	}
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	/**
+	 * @return the ethinicity
+	 */
+	public String getEthinicity() {
+		return ethinicity;
+	}
+	/**
+	 * @param ethinicity the ethinicity to set
+	 */
+	public void setEthinicity(String ethinicity) {
+		this.ethinicity = ethinicity;
+	}
+	/**
+	 * @return the disabled
+	 */
+	public String getDisabled() {
+		return disabled;
+	}
+	/**
+	 * @param disabled the disabled to set
+	 */
+	public void setDisabled(String disabled) {
+		this.disabled = disabled;
+	}
+	/**
+	 * @return the disablityDesc
+	 */
+	public String getDisablityDesc() {
+		return disablityDesc;
+	}
+	/**
+	 * @param disablityDesc the disablityDesc to set
+	 */
+	public void setDisablityDesc(String disablityDesc) {
+		this.disablityDesc = disablityDesc;
+	}
+	/**
+	 * @return the homePhone
+	 */
+	public String getHomePhone() {
+		return homePhone;
+	}
+	/**
+	 * @param homePhone the homePhone to set
+	 */
+	public void setHomePhone(String homePhone) {
+		this.homePhone = homePhone;
+	}
+	/**
+	 * @return the cellPhone
+	 */
+	public String getCellPhone() {
+		return cellPhone;
+	}
+	/**
+	 * @param cellPhone the cellPhone to set
+	 */
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the personalEmail
+	 */
+	public String getPersonalEmail() {
+		return personalEmail;
+	}
+	/**
+	 * @param personalEmail the personalEmail to set
+	 */
+	public void setPersonalEmail(String personalEmail) {
+		this.personalEmail = personalEmail;
+	}
+	/**
+	 * @return the addressLine1
+	 */
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+	/**
+	 * @param addressLine1 the addressLine1 to set
+	 */
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	/**
+	 * @return the addressLine2
+	 */
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+	/**
+	 * @param addressLine2 the addressLine2 to set
+	 */
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the zipcode
+	 */
+	public String getZipcode() {
+		return zipcode;
+	}
+	/**
+	 * @param zipcode the zipcode to set
+	 */
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	/**
+	 * @return the workPhone
+	 */
+	public String getWorkPhone() {
+		return workPhone;
+	}
+	/**
+	 * @param workPhone the workPhone to set
+	 */
+	public void setWorkPhone(String workPhone) {
+		this.workPhone = workPhone;
+	}
+	/**
+	 * @return the workExt
+	 */
+	public String getWorkExt() {
+		return workExt;
+	}
+	/**
+	 * @param workExt the workExt to set
+	 */
+	public void setWorkExt(String workExt) {
+		this.workExt = workExt;
+	}
+	/**
+	 * @return the workEmail
+	 */
+	public String getWorkEmail() {
+		return workEmail;
+	}
+	/**
+	 * @param workEmail the workEmail to set
+	 */
+	public void setWorkEmail(String workEmail) {
+		this.workEmail = workEmail;
+	}
+	/**
+	 * @return the workCellPhone
+	 */
+	public String getWorkCellPhone() {
+		return workCellPhone;
+	}
+	/**
+	 * @param workCellPhone the workCellPhone to set
+	 */
+	public void setWorkCellPhone(String workCellPhone) {
+		this.workCellPhone = workCellPhone;
+	}
+	/**
+	 * @return the emergencyLastName
+	 */
+	public String getEmergencyLastName() {
+		return emergencyLastName;
+	}
+	/**
+	 * @param emergencyLastName the emergencyLastName to set
+	 */
+	public void setEmergencyLastName(String emergencyLastName) {
+		this.emergencyLastName = emergencyLastName;
+	}
+	/**
+	 * @return the emergencyFirstName
+	 */
+	public String getEmergencyFirstName() {
+		return emergencyFirstName;
+	}
+	/**
+	 * @param emergencyFirstName the emergencyFirstName to set
+	 */
+	public void setEmergencyFirstName(String emergencyFirstName) {
+		this.emergencyFirstName = emergencyFirstName;
+	}
+	/**
+	 * @return the emergencyRelationShip
+	 */
+	public String getEmergencyRelationShip() {
+		return emergencyRelationShip;
+	}
+	/**
+	 * @param emergencyRelationShip the emergencyRelationShip to set
+	 */
+	public void setEmergencyRelationShip(String emergencyRelationShip) {
+		this.emergencyRelationShip = emergencyRelationShip;
+	}
+	/**
+	 * @return the emergencyHomePhone
+	 */
+	public String getEmergencyHomePhone() {
+		return emergencyHomePhone;
+	}
+	/**
+	 * @param emergencyHomePhone the emergencyHomePhone to set
+	 */
+	public void setEmergencyHomePhone(String emergencyHomePhone) {
+		this.emergencyHomePhone = emergencyHomePhone;
+	}
+	/**
+	 * @return the emergencyCellPhone
+	 */
+	public String getEmergencyCellPhone() {
+		return emergencyCellPhone;
+	}
+	/**
+	 * @param emergencyCellPhone the emergencyCellPhone to set
+	 */
+	public void setEmergencyCellPhone(String emergencyCellPhone) {
+		this.emergencyCellPhone = emergencyCellPhone;
+	}
+	/**
+	 * @return the emergencyEmail
+	 */
+	public String getEmergencyEmail() {
+		return emergencyEmail;
+	}
+	/**
+	 * @param emergencyEmail the emergencyEmail to set
+	 */
+	public void setEmergencyEmail(String emergencyEmail) {
+		this.emergencyEmail = emergencyEmail;
+	}
+
 	
 }
