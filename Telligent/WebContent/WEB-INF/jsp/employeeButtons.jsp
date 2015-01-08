4e4ab163266ab7fb6794a894a827c17720a45d22
reddy.telligent@gmail.com <reddy.telligent@gmail.com@2fc33e4f-b29a-dd76-131f-9e2053e58947>
2015-01-08 17:48:17 +0000

    Employee Position and Other Data UI Chanegs

--- Telligent/WebContent/WEB-INF/jsp/employeeButtons.jsp
+++ Telligent/WebContent/WEB-INF/jsp/employeeButtons.jsp
@@ -3,24 +3,34 @@
    		<td style="width: 40px"><input type="button" class="button" value="Personal" id="personal" onclick="javascript:showPersonalPage()"></td>
    		<td style="width: 40px"><input type="button" class="button" value="Compensation" id="compensation" onclick="javascript:showCompensationPage()"></td>
    		<td style="width: 40px"><input type="button" class="button" value="Employement" id="employement" onclick="javascript:showEmployementPage()"></td>
-   		<td style="width: 40px"><input type="button" class="button" value="Position" id="position"></td>
-   		<td><input type="button" class="button" value="Other Data" id="otherData"></td>
+   		<td style="width: 40px"><input type="button" class="button" value="Position" id="position" onclick="javascript:showPositionPage()"></td>
+   		<td><input type="button" class="button" value="Other Data" id="otherData" onclick="javascript:showOtherDataPage()"></td>
    		<td><span style="float: right"><a href="dashboard.htm" style="text-decoration: none;"><b>Back</b></a></span></td>
    	</tr>
 </table>
 <script type="text/javascript">
 function showPersonalPage(){
-	document.forms[0].action="showEmployeeDetails.htm?empId="+document.getElementById("employeeId").value;
+	document.forms[0].action="showEmployeeDetails.htm";
 	document.forms[0].method = "post";
 	document.forms[0].submit();
 }
 function showCompensationPage(){
-	document.forms[0].action="empCompensationPage.htm?empId="+document.getElementById("employeeId").value;
+	document.forms[0].action="empCompensationPage.htm";
 	document.forms[0].method = "post";
 	document.forms[0].submit();
 }
 function showEmployementPage(){
-	document.forms[0].action="empEmployementPage.htm?empId="+document.getElementById("employeeId").value;
+	document.forms[0].action="empEmployementPage.htm";
+	document.forms[0].method = "post";
+	document.forms[0].submit();
+}
+function showPositionPage(){
+	document.forms[0].action="empPosition.htm";
+	document.forms[0].method = "post";
+	document.forms[0].submit();
+}
+function showOtherDataPage(){
+	document.forms[0].action="empOtherData.htm";
 	document.forms[0].method = "post";
 	document.forms[0].submit();
 }
---
+++ Telligent/src/com/telligent/model/dtos/EmployeeOtherDTO.java
@@ -0,0 +1,124 @@
+package com.telligent.model.dtos;
+
+
+public class EmployeeOtherDTO extends EmployeeDTO{
+	
+	private String gender;
+	private String ethinicity;
+	private String maritalStatus;
+	private String citizenShip;
+	private String visaType;
+	private String i9ExpDate;
+	private String veteranStatus;
+	private String disability;
+	private String disabilityDesc;
+	
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	/**
+	 * @return the ethinicity
+	 */
+	public String getEthinicity() {
+		return ethinicity;
+	}
+	/**
+	 * @param ethinicity the ethinicity to set
+	 */
+	public void setEthinicity(String ethinicity) {
+		this.ethinicity = ethinicity;
+	}
+	/**
+	 * @return the maritalStatus
+	 */
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
+	/**
+	 * @param maritalStatus the maritalStatus to set
+	 */
+	public void setMaritalStatus(String maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+	/**
+	 * @return the citizenShip
+	 */
+	public String getCitizenShip() {
+		return citizenShip;
+	}
+	/**
+	 * @param citizenShip the citizenShip to set
+	 */
+	public void setCitizenShip(String citizenShip) {
+		this.citizenShip = citizenShip;
+	}
+	/**
+	 * @return the visaType
+	 */
+	public String getVisaType() {
+		return visaType;
+	}
+	/**
+	 * @param visaType the visaType to set
+	 */
+	public void setVisaType(String visaType) {
+		this.visaType = visaType;
+	}
+	/**
+	 * @return the i9ExpDate
+	 */
+	public String getI9ExpDate() {
+		return i9ExpDate;
+	}
+	/**
+	 * @param i9ExpDate the i9ExpDate to set
+	 */
+	public void setI9ExpDate(String i9ExpDate) {
+		this.i9ExpDate = i9ExpDate;
+	}
+	/**
+	 * @return the veteranStatus
+	 */
+	public String getVeteranStatus() {
+		return veteranStatus;
+	}
+	/**
+	 * @param veteranStatus the veteranStatus to set
+	 */
+	public void setVeteranStatus(String veteranStatus) {
+		this.veteranStatus = veteranStatus;
+	}
+	/**
+	 * @return the disability
+	 */
+	public String getDisability() {
+		return disability;
+	}
+	/**
+	 * @param disability the disability to set
+	 */
+	public void setDisability(String disability) {
+		this.disability = disability;
+	}
+	/**
+	 * @return the disabilityDesc
+	 */
+	public String getDisabilityDesc() {
+		return disabilityDesc;
+	}
+	/**
+	 * @param disabilityDesc the disabilityDesc to set
+	 */
+	public void setDisabilityDesc(String disabilityDesc) {
+		this.disabilityDesc = disabilityDesc;
+	}
+}
