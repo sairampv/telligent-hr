c8be10e1a05ae608f5ae1f51bccb2643d96d7303
reddy.telligent@gmail.com <reddy.telligent@gmail.com@2fc33e4f-b29a-dd76-131f-9e2053e58947>
2015-01-13 14:38:27 +0000

    Employee Compensation

---
+++ trunk/Telligent/src/com/telligent/model/dtos/CompensationDTO.java
@@ -0,0 +1,171 @@
+package com.telligent.model.dtos;
+
+
+
+public class CompensationDTO extends CommonDTO{
+	
+	private String empId;
+	private String effectiveDate;
+	private String  compActionType;
+	private String compReason;
+	private String payEntity;
+	private String payGroup;
+	private String payFreq;
+	private String lastEvaluationDate;
+	private String grade;
+	private String nextEvaluationDate;
+	private String scheduleHours;
+	private String hoursFreq;
+	private String payPeriodHours;
+	private String weeklyHours;
+	private String baseRate;
+	private String baseRateFreq;
+	private String periodRate;
+	private String hourlyRate;
+	private String defaultEarningCode;
+	private String jobGroup;
+	private String useJobRate;
+	private String performancePlan;
+	private String bonusPlan;
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getEffectiveDate() {
+		return effectiveDate;
+	}
+	public void setEffectiveDate(String effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+	public String getCompActionType() {
+		return compActionType;
+	}
+	public void setCompActionType(String compActionType) {
+		this.compActionType = compActionType;
+	}
+	public String getCompReason() {
+		return compReason;
+	}
+	public void setCompReason(String compReason) {
+		this.compReason = compReason;
+	}
+	public String getPayEntity() {
+		return payEntity;
+	}
+	public void setPayEntity(String payEntity) {
+		this.payEntity = payEntity;
+	}
+	public String getPayGroup() {
+		return payGroup;
+	}
+	public void setPayGroup(String payGroup) {
+		this.payGroup = payGroup;
+	}
+	public String getPayFreq() {
+		return payFreq;
+	}
+	public void setPayFreq(String payFreq) {
+		this.payFreq = payFreq;
+	}
+	public String getLastEvaluationDate() {
+		return lastEvaluationDate;
+	}
+	public void setLastEvaluationDate(String lastEvaluationDate) {
+		this.lastEvaluationDate = lastEvaluationDate;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getNextEvaluationDate() {
+		return nextEvaluationDate;
+	}
+	public void setNextEvaluationDate(String nextEvaluationDate) {
+		this.nextEvaluationDate = nextEvaluationDate;
+	}
+	public String getScheduleHours() {
+		return scheduleHours;
+	}
+	public void setScheduleHours(String scheduleHours) {
+		this.scheduleHours = scheduleHours;
+	}
+	public String getHoursFreq() {
+		return hoursFreq;
+	}
+	public void setHoursFreq(String hoursFreq) {
+		this.hoursFreq = hoursFreq;
+	}
+	public String getPayPeriodHours() {
+		return payPeriodHours;
+	}
+	public void setPayPeriodHours(String payPeriodHours) {
+		this.payPeriodHours = payPeriodHours;
+	}
+	public String getWeeklyHours() {
+		return weeklyHours;
+	}
+	public void setWeeklyHours(String weeklyHours) {
+		this.weeklyHours = weeklyHours;
+	}
+	public String getBaseRate() {
+		return baseRate;
+	}
+	public void setBaseRate(String baseRate) {
+		this.baseRate = baseRate;
+	}
+	public String getPeriodRate() {
+		return periodRate;
+	}
+	public void setPeriodRate(String periodRate) {
+		this.periodRate = periodRate;
+	}
+	public String getHourlyRate() {
+		return hourlyRate;
+	}
+	public void setHourlyRate(String hourlyRate) {
+		this.hourlyRate = hourlyRate;
+	}
+	public String getDefaultEarningCode() {
+		return defaultEarningCode;
+	}
+	public void setDefaultEarningCode(String defaultEarningCode) {
+		this.defaultEarningCode = defaultEarningCode;
+	}
+	public String getJobGroup() {
+		return jobGroup;
+	}
+	public void setJobGroup(String jobGroup) {
+		this.jobGroup = jobGroup;
+	}
+	public String getUseJobRate() {
+		return useJobRate;
+	}
+	public void setUseJobRate(String useJobRate) {
+		this.useJobRate = useJobRate;
+	}
+	public String getPerformancePlan() {
+		return performancePlan;
+	}
+	public void setPerformancePlan(String performancePlan) {
+		this.performancePlan = performancePlan;
+	}
+	public String getBonusPlan() {
+		return bonusPlan;
+	}
+	public void setBonusPlan(String bonusPlan) {
+		this.bonusPlan = bonusPlan;
+	}
+	public String getBaseRateFreq() {
+		return baseRateFreq;
+	}
+	public void setBaseRateFreq(String baseRateFreq) {
+		this.baseRateFreq = baseRateFreq;
+	}
+	
+	
+	
+}
